ae3a1d9d3a661e46fb555df49822d176b97aaba2
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-05-03 14:18:58 -0500

    completed jdbc lecture and created pages to show authors, quotes, and add and remove authors from database

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,10 +1,26 @@
 public class DaoFactory {
     private static Ads adsDao;
+    private static Authors authorsDao;
+    private static Quotes quotesDao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new ListAdsDao();
         }
         return adsDao;
+    }
+
+    public static Authors getAuthorsDao() {
+        if (authorsDao == null) {
+            authorsDao = new AuthorsDao();
+        }
+        return authorsDao;
+    }
+
+    public static Quotes getQuotesDao(){
+        if (quotesDao == null){
+            quotesDao = new QuotesDao();
+        }
+        return quotesDao;
     }
 }
